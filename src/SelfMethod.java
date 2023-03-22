48df7701dc826540ad12e083ed2a0b9761bea746
ashabenpatel027 <ashabenpatel027@gmail.com>
2023-03-22 13:35:44 +0000

    Commit to SelfTeaching 123

--- src/SelfMethod.java
+++ src/SelfMethod.java
@@ -1,6 +1,7 @@
 public class SelfMethod {
     public static void main(String[] args) {
 
-        System.out.println("Hello to Everyone");
+        System.out.println("Hello to Everyone" +
+                "3245");
     }
 }
